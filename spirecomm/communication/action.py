651ecf9ebd5cd2e66897940ca4217bf8e5e9743d
WangCCe <2057190330@qq.com>
2026-01-18 21:06:27 +0800

    ♻️ refactor: remove unnecessary delay in action execution
    - Eliminated the 0.01-second sleep in `execute` for improved responsiveness without impacting functionality.

--- spirecomm/communication/action.py
+++ spirecomm/communication/action.py
@@ -416,7 +416,6 @@ class CardSelectAction(Action):
         super().__init__()
 
     def execute(self, coordinator):
-        time.sleep(0.01) # sometimes we need to wait for the screen to update
         screen_type = coordinator.last_game_state.screen_type
         screen = coordinator.last_game_state.screen
         if screen_type not in [ScreenType.HAND_SELECT, ScreenType.GRID]:
